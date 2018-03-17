2e54033b43178905afe18fa6ecff83324d3be842
Nader Nasr <nadernasr7@gmail.com>
2018-03-17 11:24:42 +0200

    Self Assesment Week 1

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -9,9 +9,17 @@ var this.last = arr[arr.length - 1]
 
 function extndArr (){
 
-  var arr = []
+  var first = function (){
 
-  
+    return this.first
+
+  }
+
+  var last = function(){
+    return this.last
+  }
+
+
 
 
 
